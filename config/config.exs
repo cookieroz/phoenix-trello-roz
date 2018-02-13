6b4dc5b762f12e895695e10f7493c93ad888251b
cookieroz <cookieroz@gmail.com>
2018-02-13 08:32:49 -0800

    first commit

---
+++ config/config.exs
@@ -0,0 +1,38 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+# General application configuration
+config :phoenix_trello,
+  ecto_repos: [PhoenixTrello.Repo]
+
+# Configures the endpoint
+config :phoenix_trello, PhoenixTrello.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "UBEXR6CrMZfze9NHp6A41gDB3YMFg6FBIuqWjBojlK0BENWF37N+cKrETgVBkx6+",
+  render_errors: [view: PhoenixTrello.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: PhoenixTrello.PubSub,
+           adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# add a watcher for changes, no need to restart server
+config :phoenix_trello, PhoenixTrello.Endpoint,
+  http: [port: 4000],
+  debug_errors: true,
+  code_reloader: true,
+  cache_static_lookup: false,
+  check_origin: false,
+  watchers: [
+    node: ["node_modules/webpack/bin/webpack.js", "--watch", "--color", cd: Path.expand("../", __DIR__)]
+  ]
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env}.exs"
